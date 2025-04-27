d3b29dfb941693ed8ee1b082b0b769a51f8e49aa
SakharovIvan <i731901@gmail.com>
2025-04-28 01:06:06 +0300

    random colors added

--- src/components/three_models/geometry.ts
+++ src/components/three_models/geometry.ts
@@ -3,29 +3,46 @@ import { MyCube, MyPyramid } from "./Primitives";
 import { MyGroup } from "../../lib/group.lib";
 import { GROUPTYPES } from "../../lib/group.lib";
 export default function MyGeometry(group: MyGroup) {
-  const { position,size } = group;
+  const { position, size } = group;
 
   const positions = [];
   const normals = [];
   const uvs = [];
-  switch(group.type){
+  const colors = [];
+  const color1 = [Math.random(), Math.random(), Math.random()];
+  const color2 = [Math.random(), Math.random(), Math.random()];
+  const color3 = [Math.random(), Math.random(), Math.random()];
+  const color4 = [Math.random(), Math.random(), Math.random()];
+  const color5 = [Math.random(), Math.random(), Math.random()];
+  const color6 = [Math.random(), Math.random(), Math.random()];
+  let i = 0;
+  const randomColors = [color1, color2, color3, color4, color5, color6];
+  switch (group.type) {
     case GROUPTYPES.BOX:
       for (const vertex of MyCube(size.Length, size.Width, size.Height)) {
         positions.push(...vertex.pos);
         normals.push(...vertex.norm);
         uvs.push(...vertex.uv);
+        colors.push(...randomColors[Math.floor(i / 6)]);
+        i++;
       }
-    break
+      break;
 
     case GROUPTYPES.PYRAMID:
       for (const vertex of MyPyramid(size.Length, size.Width, size.Height)) {
         positions.push(...vertex.pos);
         normals.push(...vertex.norm);
         uvs.push(...vertex.uv);
+        if (i <= 11) {
+          colors.push(...randomColors[Math.floor(i / 3)]);
+        } else {
+          colors.push(...randomColors[4]);
+        }
+        i++;
       }
-    break
+      break;
   }
-  
+
   const geometry = new THREE.BufferGeometry();
   const positionNumComponents = 3;
   const normalNumComponents = 3;
@@ -46,14 +63,14 @@ export default function MyGeometry(group: MyGroup) {
     "uv",
     new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents)
   );
-  //geometry.setAttribute(
-  //  "color",
-  //  new THREE.BufferAttribute(new Float32Array(colors), colorNumComponents)
-  //);
-
-  const material = new THREE.LineBasicMaterial({
-    color: group.color
-  });
+  geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), colorNumComponents)
+  );
+  const material = new THREE.MeshBasicMaterial({ vertexColors: true });
+  // const material = new THREE.LineBasicMaterial({
+  //   color: group.color
+  // });
 
   const cube = new THREE.Mesh(geometry, material);
   cube.position.set(position.x, position.y, position.z);
